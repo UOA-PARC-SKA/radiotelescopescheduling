b700714ab09de08e82031905ac952987e6b54b73
Harpreet Singh <hsingh9600@gmail.com>
2021-10-09 06:16:18 +1300

    earliest setting dataset1, random dataset3, largest slew dataset 3

---
+++ all_results/dataset3/largest_slew_policy_random_preop_90_runs/LargestSlewPolicy.java
@@ -0,0 +1,36 @@
+package optimisation;
+
+import astrometrics.HorizonCoordinates;
+import observation.*;
+import observation.interference.SkyState;
+import simulation.Clock;
+
+import java.util.List;
+import java.util.Properties;
+
+public class LargestSlewPolicy extends DispatchPolicy {
+
+    public Connection findNextPath(Pointable current)
+    {
+        double maxDist = 0;
+        Connection next = null;
+
+        for (Connection conn : current.getNeighbours())
+        {
+            Pointable p = conn.getOtherTarget(current);
+            TelescopeState possState = telescope.getStateForShortestSlew(p.getHorizonCoordinates(telescope.getLocation(), Clock.getScheduleClock().getTime()));
+
+            if(possState.getSlewTime() >= maxDist)
+            {
+                next = conn;
+                maxDist = possState.getSlewTime();
+                currentTelescopeState = possState;
+            }
+
+        }
+        telescope.applyNewState(currentTelescopeState);
+        schedule.addLink(next, currentTelescopeState);
+//		updateObservable((Target)next.getOtherTarget(current));
+        return next;
+    }
+}
---
+++ all_results/dataset3/random_policy_random_preop_90_runs/RandomPolicy.java
@@ -0,0 +1,26 @@
+package optimisation;
+
+import observation.*;
+import simulation.Clock;
+import util.exceptions.OutOfObservablesException;
+
+public class RandomPolicy extends DispatchPolicy {
+
+    @Override
+    public Connection findNextPath(Pointable current) {
+
+        Connection next;
+
+        int random = (int) (Math.random() * current.getNeighbours().size());
+        Connection conn = current.getNeighbours().get(random);
+        Pointable p = conn.getOtherTarget(current);
+        TelescopeState possState = telescope.getStateForShortestSlew(p.getHorizonCoordinates(telescope.getLocation(), Clock.getScheduleClock().getTime()));
+        currentTelescopeState = possState;
+        next = conn;
+
+        telescope.applyNewState(currentTelescopeState);
+        schedule.addLink(next, currentTelescopeState);
+//		updateObservable((Target)next.getOtherTarget(current));
+        return next;
+    }
+}
